d9fe2e176014a814f85d6d89a8103b49501715a8
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-10-14 10:22:17 +0800

    Add: Activity 7 CRUD product model validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,20 +7,20 @@ class Product < ApplicationRecord
   validates :discount, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }, allow_nil: true
   has_one_attached :image
   validates :image, presence: true
-  # validate :image_validation
+  validate :image_validation
 
   private
 
-  # def image_validation
-  #   if image.attached?
-  #     unless image.content_type.in?(%w[image/jpeg image/png image/gif])
-  #       errors.add(:image, 'must be a JPEG, PNG, or GIF')
-  #     end
+  def image_validation
+    if image.attached?
+      unless image.content_type.in?(%w[image/jpeg image/png image/gif])
+        errors.add(:image, 'must be a JPEG, PNG, or GIF')
+      end
   #     #   if image.byte_size > 1.megabyte
   #     #     errors.add(:image, 'size must be less than 1 MB')
   #     #   end
   #     # else
   #     #   errors.add(:image, 'must be attached')
-  #   end
-  # end
+    end
+  end
 end
